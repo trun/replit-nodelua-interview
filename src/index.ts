6f998e5bdbe4e99c83044b7c78f4fd671f51fd32
Trevor Rundell <trevor.rundell@gmail.com>
2022-03-03 12:56:12 -0500

    hello world express app...
    just echoing back the session id for now

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello world')
+import express from 'express'
+
+const app = express()
+
+app.get('/status', (req, res) => {
+  res.status(200).send('OK')
+})
+
+app.post('/eval/:session/lua', (req, res) => {
+  const session = req.params.session
+
+  res.status(200).send({
+    session,
+  })
+})
+
+app.listen(3001, () => {
+  console.log("Server started on port 3001!")
+})
